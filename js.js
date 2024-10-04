3387c6cf4c01ed7cdd7c7dc5b96c07efcb4d9bfd
freecircuit <178741088+freecircuit@users.noreply.github.com>
2024-10-04 01:33:10 -0700

    code the grid!

--- js.js
+++ js.js
@@ -2,3 +2,18 @@ let header = document.getElementById("header");
 header.innerHTML = "<span>Etch-a-Sketch!</span>"
 let text = document.querySelector("span")
 text.style.cssText = "margin: auto"
+let grid = document .querySelector("#grid");
+let gridC = document .querySelector("#gridC");
+
+
+
+function theGrid(n){
+    grid.style.maxWidth = n * 10 + 'px';
+    gridC.style.maxHeight = n * 10 + 'px';
+    for(i=0; i < n; i++){
+    for(j = 0; j < n; j++){
+    gridC.innerHTML += '<div class="column"></div>';}
+grid.innerHTML += '<div class="row"></div>';
+}}
+
+theGrid(20);
