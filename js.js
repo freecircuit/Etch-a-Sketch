33b71035496a229ae4f84ee5b64a3c1dfd67e7dc
freecircuit <178741088+freecircuit@users.noreply.github.com>
2024-10-05 01:47:02 -0700

    add comments

--- js.js
+++ js.js
@@ -1,29 +1,32 @@
+// etch-a-sketch header
 let header = document.getElementById("header");
 header.innerHTML = "<span>Etch-a-Sketch!</span>"
 let text = document.querySelector("span")
 text.style.cssText = "margin: auto"
-const contain = document.querySelector("gridContain")
+
+// variables
 let grid = document.querySelector("#grid");
 let gridC = document.querySelector("#gridC");
 let play = document.querySelector("#play");
-let row = document.querySelector(".row")
-let column = document.querySelector(".column")
 
+// the game!
 play.addEventListener("click", (e) => {
+    // reset sketch pad
     grid.querySelectorAll(".row").forEach(div => div.remove());
     gridC.querySelectorAll(".column").forEach(div => div.remove());
+
+    // game
     let size = getSize();
     theGrid(size);
     play.textContent = "Reset";
-
 });
 
 
-
+// functions:
 function theGrid(n){
     grid.style.maxWidth = n * 10 + 'px';
     gridC.style.maxHeight = n * 10 + 'px';
-    for(i=0; i < n; i++){
+    for(i = 0; i < n; i++){
     for(j = 0; j < n; j++){
     gridC.innerHTML += '<div class="column"></div>';}
     grid.innerHTML += '<div class="row"></div>';
