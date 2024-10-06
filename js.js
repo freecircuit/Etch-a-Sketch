6ddda48a38c998b7c0f5a82bda71ef7d38d03862
freecircuit <178741088+freecircuit@users.noreply.github.com>
2024-10-06 10:49:03 -0700

    code color function

--- js.js
+++ js.js
@@ -4,10 +4,13 @@ header.innerHTML = "<span>Etch-a-Sketch!</span>"
 let text = document.querySelector("span")
 text.style.cssText = "margin: auto"
 
+
 // variables
 let grid = document.querySelector("#grid");
 let gridC = document.querySelector("#gridC");
 let play = document.querySelector("#play");
+var row = grid.querySelectorAll(".row")
+var column = gridC.querySelectorAll(".column")
 
 // the game!
 play.addEventListener("click", (e) => {
@@ -18,7 +21,13 @@ play.addEventListener("click", (e) => {
     // game
     let size = getSize();
     theGrid(size);
-    play.textContent = "Reset";
+    play.textContent = "Resize";
+
+
+
+    grid.querySelectorAll(".row").forEach(div => div.addEventListener('mousemove', color));
+    gridC.querySelectorAll(".column").forEach(div => div.addEventListener('mousemove', color));
+
 });
 
 
@@ -28,17 +37,29 @@ function theGrid(n){
     gridC.style.maxHeight = n * 10 + 'px';
     for(i = 0; i < n; i++){
     for(j = 0; j < n; j++){
-    gridC.innerHTML += '<div class="column"></div>';}
-    grid.innerHTML += '<div class="row"></div>';
+        const div = document.createElement("div");
+        div.classList.add("column");
+        gridC.appendChild(div);
+}
+const div = document.createElement("div");
+div.classList.add("row");
+grid.appendChild(div);
 }}
 
 
 function getSize(){
-    let n = prompt("Please enter number between 1 & 50 for size of sketch pad:");
+    let n = prompt("Please enter number between 1 & 100 for size of sketch pad:");
     var size = parseInt(n);
     if (size < 1 ||
-            size > 50){
+        size > 100){
             getSize()
         }
         return size
+}
+
+function color(e){
+    let colorDiv = e.target
+    colorDiv.classList.add("newColor")
+    colorDiv.style.backgroundColor = "rgb("+e.offsetX*20+","+e.offsetY*20+", 40)"
+
 }
